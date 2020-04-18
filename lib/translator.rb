bbe1aa455cac8e61661adfba88099463f983ff10
Samuel Lesser <ineedmoreplates@gmail.com>
2020-04-18 02:13:05 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,15 @@
 # require modules here
+require "pry"
+require "yaml"
 
-def load_library
+
+def load_library(emoticons)
   # code goes here
+emoticons = YAML.load_file('lib/emoticons.yml')
+
+#result = Hash[emoticons.map do |key,value| :key => Hash[:english => value[0], :japanese => value[1]] end]
+
+#pp result
 end
 
 def get_japanese_emoticon
